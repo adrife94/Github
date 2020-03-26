8b2799f34fbe2e34470292b5f1cefa8aeeab69ef
adrife94 <46284913+adrife94@users.noreply.github.com>
2020-03-26 12:37:17 +0100

    Systemout

--- app/src/main/java/edu/dam/github/MainActivity.java
+++ app/src/main/java/edu/dam/github/MainActivity.java
@@ -11,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("Hola");
+
+        System.out.println("Hola");
+
+        System.out.println("Hola");
+
+        System.out.println("Hola");
+
     }
 }
